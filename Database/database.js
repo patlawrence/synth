ad610872df7272dc8d775cc42c654c683510881f
pat-lawre <patlaw@verizon.net>
2020-12-29 23:27:48 -0500

    🛠️ Small update: Changed database password authentication method

--- Database/database.js
+++ Database/database.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const mySQL = require('mysql2/promise'); // mysql client that focuses on performance
+const caching_sha2_password = require('mysql2/lib/auth_plugins/caching_sha2_password.js');
 
 module.exports = mySQL.createConnection({
+    authPlugins: {
+        sha256_password: caching_sha2_password({})
+    }
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
