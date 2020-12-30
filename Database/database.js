61625c733c51dbbcbafececb2e5221cc481ca38f
pat-lawre <patlaw@verizon.net>
2020-12-30 00:33:55 -0500

    🛠️ Small update: added user object field to createConnection()

--- Database/database.js
+++ Database/database.js
@@ -3,6 +3,7 @@ const mySQL = require('mysql2/promise'); // mysql client that focuses on perform
 const caching_sha2_password = require('mysql2/lib/auth_plugins/caching_sha2_password.js');
 
 module.exports = mySQL.createConnection({
+    user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     charset: 'utf8mb4_unicode_ci',
