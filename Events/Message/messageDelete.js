b49bb85e371592e83aea6241aaf2e0b7a337cd16
pat-lawre <patlaw@verizon.net>
2020-12-30 20:46:39 -0500

    🛠️ Small update: Fixed syntax error in messageDelete.js

--- Events/Message/messageDelete.js
+++ Events/Message/messageDelete.js
@@ -8,8 +8,9 @@ module.exports = class extends Event {
         const channel = client.getHighlightsChannel(guildID);
         const channels = message.guild.channels;
 
+        var highlightsChannelID;
         if(channel != null)
-            const highlightsChannelID = channel.substring(2, channel.length - 1);
+            highlightsChannelID = channel.substring(2, channel.length - 1);
 
         if(message.channel.id != highlightsChannelID) {
             const highlightsChannel = channels.cache.get(highlightsChannelID);
